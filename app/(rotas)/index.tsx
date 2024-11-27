9bf6dfb3337d8907548109eb8f130dfc3d4ae7d1
Bernardo <bernardogomesrib@gmail.com>
2024-11-27 17:10:59 -0300

    depois de juntar com o que thyago e thiago fez

--- app/(rotas)/index.tsx
+++ app/(rotas)/index.tsx
@@ -2,26 +2,21 @@ import { Text, View } from '@/components/Themed';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Image } from 'expo-image';
-import { Link, router } from 'expo-router';
+import { Link } from 'expo-router';
 import { useState } from 'react';
 import { styles } from './Styles';
 export default function TabOneScreen() {
   const [email, setEmail] = useState('');
   
-  function login(){
-    router.replace('/(rotas)/home')
-  }
 
   return (
     <View style={styles.container}>
       <Image source={require('../../assets/images/salve-food.png')} style={styles.image} />
       <Input label="Email" value={email} onChangeText={setEmail} style={{width:"80%"}} keyboardType='email-address'/>
       <Input label="Senha" secureTextEntry style={{width:"80%"}}/>
-      <Link href="/recuperarSenha" asChild><Text style={styles.esqueci}>Esqueci a senha</Text></Link>
-      <Button title="Entrar" style={styles.buttonEntrar} onPress={login}/>
+      <Link href="/enviarEmail" style={styles.esqueci}><Text style={styles.esqueci}>Esqueci a senha</Text></Link>
+      <Button title="Entrar" style={styles.buttonEntrar} href="/home" />
       <Text>Não tem uma conta? <Link href="/cadastro"><Text style={styles.esqueci}>Cadastre-se</Text></Link></Text>
     </View>
   );
 }
-
-
